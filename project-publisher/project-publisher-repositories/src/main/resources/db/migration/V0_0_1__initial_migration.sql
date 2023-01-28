1f79ab39caa507facf0f17502129ec4d98467166
haykx <hayner2006@gmail.com>
2023-01-28 09:25:43 +0400

    added initial migration

---
+++ project-publisher/project-publisher-repositories/src/main/resources/db/migration/V0_0_1__initial_migration.sql
@@ -0,0 +1,27 @@
+CREATE table t_publisher
+(
+    id          UUID         NOT NULL,
+    original_id UUID         NOT NULL,
+    first_name  VARCHAR(50)  NOT NULL,
+    last_name   VARCHAR(50)  NOT NULL,
+    bio         VARCHAR(100) NOT NULL,
+    avatar      BYTEA,
+    created     TIMESTAMP    NOT NULL,
+    updated     TIMESTAMP    NOT NULL,
+    deleted     TIMESTAMP    NOT NULL,
+
+    PRIMARY KEY (id)
+);
+CREATE TABLE t_posts
+(
+    id       UUID          NOT NULL,
+    headline VARCHAR(70)   NOT NULL,
+    image    BYTEA         NOT NULL,
+    body     VARCHAR(1000) NOT NULL,
+    link     VARCHAR(40)   NOT NULL,
+    created  TIMESTAMP     NOT NULL,
+    updated  TIMESTAMP     NOT NULL,
+    deleted  TIMESTAMP     NOT NULL,
+
+    PRIMARY KEY (id)
+);
